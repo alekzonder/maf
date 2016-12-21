c99eba223eb8e9618e1afb6ec5da2a2e1b436aab
alekzonder <alekzonder@gmail.com>
2016-12-21 17:52:00 +0700

    new: Service/Di init function

---
+++ src/Service/Init/Di.js
@@ -0,0 +1,48 @@
+var path = require('path');
+
+var Di = require(path.resolve(__dirname + '/../../Di'));
+var RequestDebug = require(path.resolve(__dirname + '/../../Request/Debug'));
+
+module.exports = function (init) {
+
+    return function (logger, config, originalDi) {
+
+        return new Promise((resolve, reject) => {
+
+            var di = new Di();
+
+            di.config = config;
+
+            di.logger = logger;
+
+            Promise.resolve()
+                .then(() => {
+
+                    if (originalDi) {
+                        di.debug = new RequestDebug();
+                        di.setConnections(originalDi.getConnections());
+                    } else {
+                        return init.db(config, di);
+                    }
+
+                })
+                .then(() => {
+                    return init.models(config, di);
+                })
+                .then((models) => {
+                    di.models = models;
+                    return init.api(config, models, di);
+                })
+                .then((api) => {
+                    di.api = api;
+                    resolve(di);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+
+        });
+
+    };
+
+};
