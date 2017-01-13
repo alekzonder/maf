1c79e25294ae4afa5e6dae0bdfafba60b061e2d7
alekzonder <alekzonder@gmail.com>
2017-01-13 14:16:00 +0700

    new: Service/Logger pass global log level on creation

--- src/Service/Logger.js
+++ src/Service/Logger.js
@@ -1,6 +1,11 @@
 var log4js = require('log4js-nested');
 
-module.exports = function (name) {
+module.exports = function (name, logLevel) {
+
+    if (logLevel) {
+        log4js.setGlobalLogLevel(logLevel);
+    }
+    
     var logger = log4js.getLogger(name);
     return logger;
 };
